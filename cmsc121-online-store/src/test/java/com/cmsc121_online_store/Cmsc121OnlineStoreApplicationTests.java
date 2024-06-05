8eecbf562267212c15e073237676addaf75d186d
mmjardio <mmjardio@Up.edu.ph>
2024-06-05 21:47:38 +0800

    Add files via upload

---
+++ cmsc121-online-store/src/test/java/com/cmsc121_online_store/Cmsc121OnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmsc121_online_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cmsc121OnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
