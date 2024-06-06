e9f56425eac4d4273f3ab56d2524457708909a28
jam <jamilenetan23@gmail.com>
2024-06-06 20:00:19 +0800

    product model

---
+++ src/main/java/com/MP_121/model/ProductModel.java
@@ -0,0 +1,56 @@
+package com.MP_121.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class ProductModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
+
